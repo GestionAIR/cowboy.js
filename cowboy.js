53df21b468134ebe5ac3cdacc7651182bafab398
Killian Blais <contact@killianblais.fr>
2013-05-24 11:04:54 +0200

    Add new generic option system

--- cowboy.js
+++ cowboy.js
@@ -1,2 +1,14 @@
 // Define cowboy.js namespace
 var cowboy = {};
+
+cowboy.Options = new Class({
+	Implements:Options,
+	setElementOptions: function(options, element, listProperties) {
+		listProperties.each(function(property) {
+			if(element.getProperty('data-' + property)) {
+				options[property] = element.getProperty('data-' + property);
+			}
+		});
+		this.setOptions(options);
+	}
+});
