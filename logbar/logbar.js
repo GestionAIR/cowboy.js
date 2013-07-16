c39489187a8c91ea4a517b63b5f3bfbc8f76ee46
Killian Blais <contact@killianblais.fr>
2013-07-16 15:19:50 +0200

    Refactoring the Logbar component

--- logbar/logbar.js
+++ logbar/logbar.js
@@ -3,37 +3,77 @@
  * Displays a status bar in the bottom of the screen
  */
 cowboy.LogBar = new Class({
-	initialize: function() {
+	Implements: cowboy.Options,
+	options: {
+		transition: 'Expo.easeOut',
+		displayOnStartup: false,
+		logbarHeight: 56,
+		idContent: 'log'
+	},
+
+	/**
+	 * Constructor
+	 * @param  {Object}	options	Options
+	 */
+	initialize: function(options) {
+		this.options = Object.merge(this.options, options);
+
 		// status = false because the logbar is not displayed at startup
 		this.status = false;
-	}
-	, _show: function(duration) {
-		// If the logbar is already visible, do nothing
-		if (!this.status) {
-			// Define animation
-			var fx = new Fx.Morph($('log-bar'), { duration: '1000', transition: Fx.Transitions.Expo.easeOut });
 
-			// Start animation
-			fx.start({'bottom': [-56, 0]});
+		if (this.options.displayOnStartup) {
+			this.show({text: options.text, duration: options.duration });
+		}
+	},
+
+	/**
+	 * Display the logbar
+	 * @param  {Object}	options	Options
+	 */
+	show: function(options) {
+		if (this.status) {
+			if (options.text !== undefined) {
+				$(this.options.idContent).set('html', options.text);
+			}
+			if (options.duration !== undefined) {
+				(function() { LOGBAR.hide();}).delay(options.duration);
+			}
+		}
+		else {
+			if (options.text !== undefined) {
+				$(this.options.idContent).set('html', options.text);
+			}
+
+			// Define animation
+			new Fx.Morph($('log-bar'), {
+				duration: '1000',
+				transition: window['Fx.Transitions.' + this.options.transition]
+			}).start({'bottom': [-this.options.logbarHeight, 0]});
 
 			// Write that the logbar is up
 			this.status = true;
 
 			// If a parameter was specified, hide the logbar after x milliseconds
-			if (duration > 0) (function () { LOGBAR._hide(); }).delay(duration);
+			if (options.duration) {
+				(function () { LOGBAR.hide();}).delay(options.duration);
+			}
 		}
-	}	
-	, _hide: function() {
+	},
+
+	/**
+	 * Hide the logbar
+	 */
+	hide: function() {
 		// If the logbar is not visible, do nothing
 		if (this.status) {
 			// Define animation
-			var fx = new Fx.Morph($('log-bar'), { duration: '1000', transition: Fx.Transitions.Expo.easeOut });
-
-			// Start animation
-			fx.start({'bottom': [0, -56]});
+			new Fx.Morph($('log-bar'), {
+				duration: '1000',
+				transition: window['Fx.Transitions.' + this.options.transition]
+			}).start({'bottom': [0, -this.options.logbarHeight]});
 
 			// Write that the logbar is down
-			this.status = false
+			this.status = false;
 		}
 	}
 });
